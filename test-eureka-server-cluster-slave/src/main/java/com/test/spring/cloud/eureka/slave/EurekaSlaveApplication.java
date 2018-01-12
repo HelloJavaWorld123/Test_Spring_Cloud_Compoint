d244b57d72ba76595ddb31a18724c2fe09f15bd6
HelloJavaWorld123 <1806146700@qq.com>
2018-01-12 16:57:07 +0800

    集成自定义logback日志文件在本地测试

--- test-eureka-server-cluster-slave/src/main/java/com/test/spring/cloud/eureka/slave/EurekaSlaveApplication.java
+++ test-eureka-server-cluster-slave/src/main/java/com/test/spring/cloud/eureka/slave/EurekaSlaveApplication.java
@@ -1,6 +1,5 @@
-package com.test.spring.cloud.eurk.server;
+package com.test.spring.cloud.eureka.slave;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,20 +7,18 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * ======================
  * Created By User: RXK
- * Date: 2018/1/8
- * Time: 9:55
+ * Date: 2018/1/11
+ * Time: 15:56
  * Version: V1.0
- * Description:启动注册服务中心类
+ * Description: 启动类
  * ======================
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class EurkServerApplication
+public class EurekaSlaveApplication
 {
-
 	public static void main(String[] args)
 	{
-		new SpringApplicationBuilder(EurkServerApplication.class).web(true).run(args);
+		new SpringApplicationBuilder(EurekaSlaveApplication.class).web(true).run(args);
 	}
-
 }
---
+++ test-eurk-server-cluster-master/src/main/java/com/test/spring/cloud/eureka/server/EurkServerApplication.java
@@ -0,0 +1,26 @@
+package com.test.spring.cloud.eureka.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * ======================
+ * Created By User: RXK
+ * Date: 2018/1/8
+ * Time: 9:55
+ * Version: V1.0
+ * Description:启动注册服务中心类
+ * ======================
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurkServerApplication
+{
+
+	public static void main(String[] args)
+	{
+		new SpringApplicationBuilder(EurkServerApplication.class).web(true).run(args);
+	}
+
+}
