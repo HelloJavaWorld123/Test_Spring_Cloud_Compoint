189ae62585f6a4320e0053b54094bc3947acb013
HelloJavaWorld123 <1806146700@qq.com>
2018-01-13 18:35:39 +0800

    修改项目结构

---
+++ test-eureka-client/src/main/java/com/test/eureka/client/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package com.test.eureka.client;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * ======================
+ * Created By User: RXK
+ * Date: 2018/1/13
+ * Time: 15:54
+ * Version: V1.0
+ * Description:
+ * ======================
+ */
+@SpringBootApplication
+public class EurekaClientApplication
+{
+	public static void main(String[]args){
+		new SpringApplicationBuilder(EurekaClientApplication.class).web(true)
+				.run(args);
+	}
+}
